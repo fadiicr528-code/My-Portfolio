0443338156b09e381a37fc47462b239935a627af
Shah fahad <fadiicr528@gmail.com>
2026-07-14 19:16:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿const darkBtn = document.getElementById("darkBtn");
+
+darkBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        darkBtn.innerHTML="Light Mode";
+    }
+    else{
+        darkBtn.innerHTML="Dark Mode";
+    }
+
+});
